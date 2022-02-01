8bf02c07177c60037a5163b5cca6b3ec148b80e2
oranmoshai <oran.moshai@aquasec.com>
2022-02-01 17:55:26 +0200

    Update readme for sarif deprecate https://github.com/aquasecurity/trivy/discussions/1571

--- entrypoint.sh
+++ entrypoint.sh
@@ -140,7 +140,7 @@ returnCode=$?
 # This is a feature, not a bug :)
 if [[ ${template} == *"sarif"* ]]; then
   echo "Building SARIF report with options: ${SARIF_ARGS}" "${artifactRef}"
-  trivy --quiet ${scanType} --format template --template ${template} --output ${output} $SARIF_ARGS ${artifactRef}
+  trivy --quiet ${scanType} --format sarif --output ${output} $SARIF_ARGS ${artifactRef}
 fi
 
 exit $returnCode
