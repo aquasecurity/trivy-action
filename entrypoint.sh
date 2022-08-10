e81cb6b8b016bcc150b6bb737132a3a9ed0e11a6
chejn <41510168+chejn@users.noreply.github.com>
2022-08-11 09:16:16 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -186,7 +186,7 @@ fi
 if [[ "${format}" == "github" ]]; then
   if [[ "$(echo $githubPAT | xargs)" != "" ]]; then
     printf "\n Uploading GitHub Dependency Snapshot"
-    curl -u "${githubPAT}" -H 'Content-Type: application/json' 'https://api.github.com/repos/'$GITHUB_REPOSITORY'/dependency-graph/snapshots' -d @./$(echo $output | xargs)
+    curl -H 'Content-Type: application/json' -H 'Authorization: token '$githubPAT'' 'https://api.github.com/repos/'$GITHUB_REPOSITORY'/dependency-graph/snapshots' -d @./$(echo $output | xargs)
   else
     printf "\n Failing GitHub Dependency Snapshot. Missing github-pat"
   fi
