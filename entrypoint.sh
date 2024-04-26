aquasecurity/trivy-action

commit ee6a4f5af1dbe22ccc11905a61abd2db301d532c

fix: 🐛 allow trivy-config and other options to be used together (#338)

entrypoint.sh
@@ -195,7 +195,7 @@ if [ "${format}" == "sarif" ] && [ "${limitSeveritiesForSARIF}" != "true" ]; the
   trivy --quiet ${scanType} --format sarif --output ${output} $SARIF_ARGS ${artifactRef}
 elif [ $trivyConfig ]; then
    echo "Running Trivy with trivy.yaml config from: " $trivyConfig
-   trivy --config $trivyConfig ${scanType} ${artifactRef}
+   trivy --config $trivyConfig ${scanType} ${ARGS}" ${artifactRef}
 else
    echo "Running trivy with options: trivy ${scanType} ${ARGS}" "${artifactRef}"
    echo "Global options: " "${GLOBAL_ARGS}"

commit acbc5e4dde89751168b2289d43c0c4506ed52c8e

Correct issue introduced in commit ee6a4f5 (related to #343)

entrypoint.sh
@@ -195,7 +195,7 @@ if [ "${format}" == "sarif" ] && [ "${limitSeveritiesForSARIF}" != "true" ]; the
   trivy --quiet ${scanType} --format sarif --output ${output} $SARIF_ARGS ${artifactRef}
 elif [ $trivyConfig ]; then
    echo "Running Trivy with trivy.yaml config from: " $trivyConfig
-   trivy --config $trivyConfig ${scanType} ${ARGS}" ${artifactRef}
+   trivy --config $trivyConfig ${scanType} ${ARGS} ${artifactRef}
 else
    echo "Running trivy with options: trivy ${scanType} ${ARGS}" "${artifactRef}"
    echo "Global options: " "${GLOBAL_ARGS}"