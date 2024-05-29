b44ceb1fba8029c3be7e0f2a8b8d4d6c67eafbc3
Martin Costello <martin@martincostello.com>
2024-05-29 15:37:21 +0100

    Use environment variable for GitHub API
    Use GITHUB_API_URL for the request to the dependency snapshot endpoint so requests work in GitHub Enterprise Server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -207,7 +207,7 @@ set -e
 if [[ "${format}" == "github" ]]; then
   if [[ "$(echo $githubPAT | xargs)" != "" ]]; then
     printf "\n Uploading GitHub Dependency Snapshot"
-    curl -H 'Accept: application/vnd.github+json' -H "Authorization: token $githubPAT" 'https://api.github.com/repos/'$GITHUB_REPOSITORY'/dependency-graph/snapshots' -d @./$(echo $output | xargs)
+    curl -H 'Accept: application/vnd.github+json' -H "Authorization: token $githubPAT" ''$GITHUB_API_URL'/repos/'$GITHUB_REPOSITORY'/dependency-graph/snapshots' -d @./$(echo $output | xargs)
   else
     printf "\n Failing GitHub Dependency Snapshot. Missing github-pat"
   fi
